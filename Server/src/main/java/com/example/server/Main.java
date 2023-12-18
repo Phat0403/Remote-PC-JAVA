bb687d237e7fce1b824e479884ad2c348c9be979
phnguynmkhoi <pnmk0811@gmail.com>
2023-12-18 15:39:19 +0700

    small changes

--- Server/src/main/java/com/example/server/Main.java
+++ Server/src/main/java/com/example/server/Main.java
@@ -1,8 +1,4 @@
 package com.example.server;
-import com.example.server.features.list.ListPrc;
-import com.example.server.features.list.RunApp;
-
-import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
